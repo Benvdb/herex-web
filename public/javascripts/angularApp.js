d7519518599c24c0de0440711e4d10700977467d
Benvdb <ben.vandenbossche.t1141@student.hogent.be>
2017-08-11 12:32:33 +0200

    initial commit, basisproject

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,233 @@
+var app = angular.module('todolijst', ['ui.router'])
+
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider,$urlRouterProvider) {
+    
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl',
+        resolve: {
+          postPromise: ['todos', function(todos){
+            return todos.getAll();
+    }]
+  }
+      })
+      .state('todos', {
+  url: '/todos/{id}',
+  templateUrl: '/todoDetails.html',
+  controller: 'TodoDetailCtrl',
+  resolve: {
+    todo: ['$stateParams', 'todos', function($stateParams, todos) {
+      return todos.get($stateParams.id);
+    }]
+  }
+})
+.state('login', {
+  url: '/login',
+  templateUrl: '/login.html',
+  controller: 'AuthCtrl',
+  onEnter: ['$state', 'auth', function($state, auth){
+    if(auth.isLoggedIn()){
+      $state.go('home');
+    }
+  }]
+})
+.state('register', {
+  url: '/register',
+  templateUrl: '/register.html',
+  controller: 'AuthCtrl',
+  onEnter: ['$state', 'auth', function($state, auth){
+    if(auth.isLoggedIn()){
+      $state.go('home');
+    }
+  }]
+});
+
+      $urlRouterProvider.otherwise('home');
+  }]);
+
+
+app.factory('todos', ['$http','auth',function($http,auth){
+  var o =  {
+    todos: []
+  };
+  
+      o.getAll = function() {
+      return $http.get('/todos').success(function(data){
+        angular.copy(data, o.todos);
+      });
+    };
+    o.create = function(todo) {
+      return $http.post('/todos', todo, {
+        headers: {Authorization: 'Bearer '+auth.getToken()}
+      }).success(function(data){
+        o.todos.push(data);
+      });
+    };
+    o.delete = function(todo) {
+		return $http.delete('/todos/' + todo._id).success(function(data) {
+			angular.copy(data, o.todos);
+		});
+  }
+  
+  o.update = function(todo){
+    console.log("o.update " + todo.name);
+    return $http.put('/todos/'+ todo._id, todo).then(function(data){
+      angular.copy(todo);
+    });
+  }
+
+
+o.get = function(id) {
+  return $http.get('/todos/' + id).then(function(res){
+    return res.data;
+  });
+};
+
+  return o;
+}]);
+
+app.controller('MainCtrl', [
+  '$scope',
+  'todos',
+  'auth',
+  function($scope,todos,auth){
+
+    $scope.editing = [];
+    
+    $scope.todos = todos.todos;
+
+      $scope.isLoggedIn = auth.isLoggedIn;
+
+    $scope.addTodo = function(){
+    if($scope.newTodo === '') { return; }
+    todos.create({
+      name: $scope.newTodo,
+      completed: false,
+      note:''
+    });
+    $scope.newTodo = '';
+  };
+
+  $scope.remove = function(index){
+    var todo = $scope.todos[index]
+
+    console.log(todo);
+    todos.delete(todo);
+
+
+  }
+
+  $scope.edit = function(index){
+            $scope.editing[index] = angular.copy($scope.todos[index]);
+          }
+
+  $scope.update = function(index){
+    var todo = $scope.todos[index];
+    console.log("scope.update " + todo.name);
+    todos.update(todo);
+    $scope.editing[index] = false;
+  }
+  }]);
+
+app.controller('TodoDetailCtrl', ['$scope','todos','todo','auth','$state', function($scope,todos,todo,auth,$state){
+$scope.todo = todo;
+console.log("$scope.todo " + $scope.todo);
+
+$scope.addNote = function(){
+console.log("updateFunc");
+            todos.update(todo);
+            $state.go('home');
+}
+
+
+}]);
+
+
+
+app.factory('auth', ['$http', '$window', function($http, $window){
+   var auth = {};
+
+   auth.saveToken = function (token){
+  $window.localStorage['todo-lijst-token'] = token;
+};
+
+auth.getToken = function (){
+  return $window.localStorage['todo-lijst-token'];
+}
+
+auth.isLoggedIn = function(){
+  var token = auth.getToken();
+
+  if(token){
+    var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+    return payload.exp > Date.now() / 1000;
+  } else {
+    return false;
+  }
+};
+
+auth.currentUser = function(){
+  if(auth.isLoggedIn()){
+    var token = auth.getToken();
+    var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+    return payload.username;
+  }
+};
+auth.register = function(user){
+  return $http.post('/register', user).success(function(data){
+    auth.saveToken(data.token);
+  });
+};
+auth.logIn = function(user){
+  return $http.post('/login', user).success(function(data){
+    auth.saveToken(data.token);
+  });
+};
+
+auth.logOut = function(){
+  $window.localStorage.removeItem('todo-lijst-token');
+};
+  return auth;
+}])
+
+
+app.controller('AuthCtrl', [
+'$scope',
+'$state',
+'auth',
+function($scope, $state, auth){
+  $scope.user = {};
+
+  $scope.register = function(){
+    auth.register($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+
+  $scope.logIn = function(){
+    auth.logIn($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+}])
+
+app.controller('NavCtrl', [
+'$scope',
+'auth',
+function($scope, auth){
+  $scope.isLoggedIn = auth.isLoggedIn;
+  $scope.currentUser = auth.currentUser;
+  $scope.logOut = auth.logOut;
+}]);
+
---
+++ routes/index.js
@@ -0,0 +1,122 @@
+var express = require('express');
+var router = express.Router();
+
+var Todo = require('../models/Todos.js');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+
+
+var mongoose = require('mongoose');
+var passport = require('passport');
+var User = mongoose.model('User');
+var jwt = require('express-jwt');
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
+
+
+router.param('todo', function(req, res, next, id) {
+  var query = Todo.findById(id);
+
+  query.exec(function (err, todo){
+    if (err) { return next(err); }
+    if (!todo) { return next(new Error('can\'t find todo')); }
+
+    req.todo = todo;
+    return next();
+  });
+});
+
+
+router.post('/register', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+
+  user.setPassword(req.body.password)
+
+  user.save(function (err){
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()})
+  });
+
+});
+router.post('/login', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  passport.authenticate('local', function(err, user, info){
+    if(err){ return next(err); }
+
+    if(user){
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
+});
+
+
+router.get('/todos', function(req, res, next) {
+  console.log("get/todos");
+  Todo.find(function (err, todos) {
+    if (err) {console.log("error get todos"); return next(err);}
+    console.log(todos);
+    res.json(todos);
+  });
+});
+
+
+router.post('/todos' ,function(req, res, next) {
+  var todo = new Todo(req.body);
+  todo.save(function(err, todo){
+    if(err){ return next(err); }
+
+    res.json(todo);
+  });
+});
+
+
+router.get('/todos/:id', function(req, res, next) {
+  Todo.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+router.put('/todos/:todo', function(req,res,next){
+  console.log(JSON.stringify(req.body));
+  console.log(req.body.name);
+  console.log(req.body.completed);
+  console.log(req.body._id);
+
+  Todo.findByIdAndUpdate(req.body._id, req.body, function (err, todo) {
+    if (err) return next(err);
+    res.json(todo);
+  });
+ 
+});
+
+
+router.delete('/todos/:todo', function(req, res) {
+	Todo.remove({
+		_id: req.params.todo
+	}, function(err, todo) {
+		if (err) { return next(err); }
+		
+		Todo.find(function(err, todos) {
+			if (err) { return next(err); }
+			
+			res.json(todos);
+		});
+	});
+});
